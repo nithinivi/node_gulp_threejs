f338201077ff3de739ed2511f4b14ece6ecf9cca
nithinivi <nithinnivi@gmail.com>
2019-12-26 13:46:16 +0530

    rotating a cube

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,33 @@ var camera = new THREE.PerspectiveCamera(
 
 camera.position.z = 5;
 
+// appending contents to dom  /////////////////////////////////////////////////
 
-// Create the canvas with a renderer and tell the
-// renderer to clean up jagged aliased lines
 var renderer = new THREE.WebGLRenderer({antialias: true});
-
-// Specify the size of the canvas
 renderer.setSize( window.innerWidth, window.innerHeight );
-
-// Add the canvas to the DOM
 document.body.appendChild( renderer.domElement );
+
+// adding texture /////////////////////////////////////////////////////////////
+var loader = new THREE.TextureLoader();
+var url = 'https://s3.amazonaws.com/duhaime/blog/tsne-webgl/assets/cat.jpg';
+var material = new THREE.MeshLambertMaterial({
+    map: loader.load(url)
+});
+
+
+// adding geomtry //////////////////////////////////////////////////////////////
+var geometry = new THREE.PlaneGeometry(10, 10*.75);
+var mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(100,0,0)
+scene.add(mesh);
+
+
+// adding light ///////////////////////////////////////////////////////////////
+var light = new THREE.PointLight(0xffffff, .7, 0);
+light.position.set(1, 1, 100);
+
+scene.add(light)
+
+
+
+renderer.render(scene, camera);
